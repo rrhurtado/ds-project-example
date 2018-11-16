28bb8fe62ed44ab559917ade413499b0b582ed07
rodrigo <rrhurtado@uc.cl>
2018-11-16 13:02:19 -0300

    feature(project-example): base project example

---
+++ src/core.py
@@ -0,0 +1,6 @@
+def print_hello_world():
+  print("Hello World")
+
+def print_coded_string():
+  print('eJyNkFEKwDAIQ/93isC7/x3LyIJullHrR1BfJSIJPUHTlmiUPHbxC7L56wNCgZAxv3SjDWIxsoOb\nzMaBwyHYPJ5sVPNYxXism74vcIsFZlYyrg=='.decode(
+      'base64').decode('zip'))
